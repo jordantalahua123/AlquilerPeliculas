cf110b00e18912806d5170694a45e86986129305
Diego Tutin <diegotutin3255@gmail.com>
2022-08-19 11:13:24 -0500

    Version Diego 19/08/2022

--- view/generos/create.php
+++ view/generos/create.php
@@ -7,14 +7,14 @@
                     <b>Registrar Género</b>
                 </div>
             </div>
-            <form>
+            <form action = "../../controller/generos/create.php" method ="POST">
             <div class="mb-3">
                 <label for="nombreSocio" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="gen_nombre">
+                <input type="text" class="form-control" id="gen_nombre" name ="gen_nombre" required>
                 <br>
-            <button type="submit" class="btn btn-primary">Enviar</button>
+            <button type="submit" class="btn btn-primary">Agregar</button>
             </form>
         </div>
     </div>
-    
+</div>
     <?php  include '../template/footer.php'?>
--- view/generos/update.php
+++ view/generos/update.php
@@ -1,4 +1,5 @@
 <?php  include '../template/header.php'?>
+<?php  include '../../controller/generos/update.php'?>
 
     <div class="row">
         <div class="col-3"></div>
@@ -8,10 +9,17 @@
                     <b>Actualizar Género</b>
                 </div>
             </div>
-            <form>
+
+            <form action = "../../controller/generos/update.php" method = "POST">
+            <?php
+                    $row = $result->fetch_assoc();
+            ?>
             <div class="mb-3">
-                <label for="nombreSocio" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="gen_nombre">
+                <label for="gen_nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="gen_nombre" name = "gen_nombre"
+                value = "<?php echo $row['gen_nombre']?>" required >
+                <input type="hidden" class="form-control" name = "gen_id"
+                value = "<?php echo $row['gen_id']?>">
                 <br>
             <button type="submit" class="btn btn-success">Actualizar</button>
             </form>
