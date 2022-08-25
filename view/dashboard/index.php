32ff8b35ec5c357636b411730796ae019176c8ed
jordantalahua123 <jordantalahua@gmail.com>
2022-08-25 10:20:35 -0500

    Mas cambios varios

--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -1,16 +1,15 @@
-<?php  include '../template/header.php'?>
-<?php  include '../../controller/dashboard/index.php'?>
-<!-- Main content -->
+<?php include '../template/header.php' ?>
+<?php include '../../controller/dashboard/index.php' ?>
 <section class="content">
     <div class="row">
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="info-box">
-                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-film"></i></span>
 
                 <div class="info-box-content">
-                    <span class="info-box-text">Total Peliculas</span>
+                    <span class="info-box-text">Total Películas</span>
                     <span class="info-box-number">
-                        <?php echo $total_peliculas ?>
+                        <?php echo $total_pel?>
                     </span>
                 </div>
                 <!-- /.info-box-content -->
@@ -18,43 +17,34 @@
             <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
-            <div class="info-box mb-3">
-                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
-                <div class="info-box-content">
-                    <span class="info-box-text">Total Socios</span>
-                    <span class="info-box-number"><?php echo $total_socios ?> </span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
         <!-- /.col -->
 
         <!-- fix for small devices only -->
         <div class="clearfix hidden-md-up"></div>
 
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="info-box mb-3">
                 <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
                 <div class="info-box-content">
-                    <span class="info-box-text">Sales</span>
-                    <span class="info-box-number">760</span>
+                    <span class="info-box-text">Total Alquileres</span>
+                    <span class="info-box-number">
+                        <?php echo $total_alq ?>
+                    </span>
                 </div>
                 <!-- /.info-box-content -->
             </div>
             <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="info-box mb-3">
                 <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
                 <div class="info-box-content">
-                    <span class="info-box-text">New Members</span>
-                    <span class="info-box-number">2,000</span>
+                    <span class="info-box-text">Total socios</span>
+                    <span class="info-box-number"><?php echo $total_soc ?></span>
                 </div>
                 <!-- /.info-box-content -->
             </div>
@@ -62,43 +52,56 @@
         </div>
         <!-- /.col -->
     </div>
-    <div class="card">
-        <div class="card-header border-0">
-            <div class="d-flex justify-content-between">
-                <h3 class="card-title">Sales</h3>
-                <a href="javascript:void(0);">View Report</a>
-            </div>
-        </div>
-        <div class="card-body">
-            <div class="d-flex">
-                <p class="d-flex flex-column">
-                    <span class="text-bold text-lg">$18,230.00</span>
-                    <span>Sales Over Time</span>
-                </p>
-                <p class="ml-auto d-flex flex-column text-right">
-                    <span class="text-success">
-                        <i class="fas fa-arrow-up"></i> 33.1%
-                    </span>
-                    <span class="text-muted">Since last month</span>
-                </p>
-            </div>
-            <!-- /.d-flex -->
+    <div class="row justify-content-center">
+        <div class="col-lg-8 ">
+            <div class="card ">
+                <div class="card-header border-0 ">
+                    <div class="d-flex justify-content-center">
+                        <H3 class="card-title text-bold text-lg">ALQUILERES</H3>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex ">
+                        <p class="d-flex flex-column">
+                            <span class="text-bold text-lg ">Total: $<?php echo $total_alquileres?></span>
+                        </p>
+                    </div>
+                    <!-- /.d-flex -->
 
-            <div class="position-relative mb-4">
-                <canvas id="sales-chart" height="200"></canvas>
-            </div>
-
-            <div class="d-flex flex-row justify-content-end">
-                <span class="mr-2">
-                    <i class="fas fa-square text-primary"></i> This year
-                </span>
-
-                <span>
-                    <i class="fas fa-square text-gray"></i> Last year
-                </span>
+                    <div class="position-relative mb-4">
+                        <canvas id="sales-chart" height="200"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </section>
-<!-- /.content -->
-<?php  include '../template/footer.php'?>
+<script>
+const labels = <?php echo $meses ?>
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'Alquileres',
+        data: <?php echo $alq_meses ?>,
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+    }]
+};
+
+const config = {
+    type: 'doughnut',
+    data: data,
+};
+</script>
+<script>
+const myChart = new Chart(
+    document.getElementById('sales-chart'),
+    config
+);
+</script>
+<?php include '../template/footer.php' ?>
